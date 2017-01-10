032978603675d18cee341894802a3263522fea39
ktedeveloper <khrisna.elumba@gmail.com>
2017-01-11 02:10:59 +0800

    finished the tutorial

--- includes/js/script.js
+++ includes/js/script.js
@@ -8,3 +8,25 @@ Updated: August 2013
 Notes:	 Hand coded for Udemy.com
 
 */
+
+$(function() {
+
+  $('#alertMe').click(function(e) {
+
+    e.preventDefault();
+
+    $('#successAlert').slideDown();
+
+  });
+
+  //disable default function of a link
+  $('a.pop').click(function(e) {
+    e.preventDefault();
+  });
+
+  $('a.pop').popover();
+
+  //selecting attributes
+  $('[rel="tooltip"]').tooltip();
+
+});
